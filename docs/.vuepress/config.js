d713e33e2e3cdcb4e318f7f4b3f0beeca9a8bd46
wingkong1978 <wingkong1978@163.com>
2019-08-21 11:16:23 +0800

    init version

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    title: '人强不需要马壮个人主页',
+    description: '人强不需要马壮的博客',
+    head: [
+        ['link', { rel: 'icon', href: '/img/logo.ico' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ],
+    themeConfig: {
+        nav: [
+            { text: '主页', link: '/' },
+            { text: '博文',
+                items: [
+                    { text: 'Android', link: '/android/' },
+                    { text: 'ios', link: '/ios/' },
+                    { text: 'Web', link: '/web/' }
+                ]
+            },
+            { text: '关于', link: '/about/' },
+            { text: 'Github', link: 'https://www.github.com/codeteenager' },
+        ],
+        sidebar: {
+            '/android/': [
+                "",
+                "android1"
+            ],
+            "/ios/":[
+                "",
+                "ios1"
+            ],
+            "/web/":[
+                "",
+                "web1"
+            ],
+        },
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+    }
+
+}
